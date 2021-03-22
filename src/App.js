5f195d725d8a2bf6a7f4c9a686008d0dffcb7785
Anvesh Shrivastava <shrivastava_av@India.XoriantCorp.com>
2021-03-22 10:10:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/employee/Dashboard';
 import EmployeeItem from './components/employee/EmployeeItem';
